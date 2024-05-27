a7188dee8deec7e7ad42eccc89a9f74bca9612f7
Cauan Riffel <83404051+cauan-riffel@users.noreply.github.com>
2024-05-27 14:03:56 -0300

    Update main.js

--- learning/main.js
+++ learning/main.js
@@ -1,7 +1,7 @@
 "use strict"
 const ctx=document.getElementById("canva").getContext("2d");
 
-function throttle(func, limit) {
+function throttle(func, limit,endfunc=null) {
   let inThrottle;
   return function() {
     const args = arguments;
@@ -11,41 +11,58 @@ function throttle(func, limit) {
       inThrottle = true;
       setTimeout(() =>{
       	inThrottle = false;
-      	func();
+      	endfunc&&endfunc();
       }, limit);
     }
   }
 }
 
-function resizeCanvas(){
+function handleResize(){
 	let l=circles.length;
 	while(l--){
 		circles.pop();
 	}
 
-	ctx.canvas.style.width = innerWidth*0.95 + 'px';
-	ctx.canvas.style.height = innerHeight*0.9 + 'px';
-	ctx.canvas.width = innerWidth*0.95;
-	ctx.canvas.height = innerHeight*0.9;
+	ctx.canvas.style.width = innerWidth + 'px';
+	ctx.canvas.style.height = innerHeight + 'px';
+	w=ctx.canvas.width = innerWidth;
+	h=ctx.canvas.height = innerHeight;
 
 	for(let i=0;i<100;i++){
-	circles.push(
-		new Circle(
-			startRadius+Math.floor(Math.random()*(ctx.canvas.width-startRadius*2)),
-			startRadius+Math.floor(Math.random()*(ctx.canvas.height-startRadius*2)),
-			Math.floor((Math.random()-0.5)*10)||1,
-			Math.floor((Math.random()-0.5)*10)||1,
-			"rgb("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+")",
-			startRadius
-	))
+		circles.push(
+			new Circle(
+				startRadius+Math.floor(Math.random()*(w-startRadius*2)),
+				startRadius+Math.floor(Math.random()*(h-startRadius*2)),
+				Math.floor((Math.random()-0.5)*10)||1,
+				Math.floor((Math.random()-0.5)*10)||1,
+				"rgb("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+")",
+				startRadius,
+				4.5
+			)
+		)
+	}
+	animateCircles();
 }
+window.addEventListener('resize',throttle(handleResize,250,handleResize));
+
+function handleMouseMove(e){
+	pointer.x=e.x;
+	pointer.y=e.y;
 }
+ctx.canvas.addEventListener('mousemove',throttle(handleMouseMove,50));
 
-window.addEventListener('resize',throttle(resizeCanvas,250));
+ctx.canvas.addEventListener('mouseout',(e)=>{
+	pointer.x=-1000;
+	pointer.y=-1000;
+})
 
-
-
-console.log(ctx);
+document.addEventListener("visibilitychange", function() {
+	if (document.visibilityState === 'hidden') {
+		pause=true;
+	} else {
+		pause=false;
+	}
+});
 
 // retangle
 /*
@@ -82,7 +99,7 @@ ctx.canvas.onmousemove=(e)=>{
 }*/
 
 class Circle{
-	constructor(x,y,dx,dy,color,radius){
+	constructor(x,y,dx,dy,color,radius,factor=15){
 		this.x=x;
 		this.y=y;
 		this.dx=dx;
@@ -90,17 +107,36 @@ class Circle{
 
 		this.col=color;
 		this.r=radius;
+		this.minR=radius;
+		this.f=factor;
 	}
 
-	move(ctx){
-
+	move(ctx,pointer){
 		this.draw(ctx);
 
-		if(this.x+this.dx-this.r<0||this.x+this.dx+this.r>ctx.canvas.width){
+		if(this.x+this.dx-this.r<0||this.x+this.dx+this.r>w){
 			this.dx=-this.dx;
 		}
-		if(this.y+this.dy-this.r<0||this.y+this.dy+this.r>ctx.canvas.height){
+		if(this.y+this.dy-this.r<0||this.y+this.dy+this.r>h){
 			this.dy=-this.dy;
+		}
+		const minRPlusFactor=this.minR*this.f*1.4;
+		const inRange=
+			pointer.x>=this.x-minRPlusFactor &&
+			pointer.x<=this.x+minRPlusFactor &&
+			pointer.y>=this.y-minRPlusFactor &&
+			pointer.y<=this.y+minRPlusFactor;
+
+		if(inRange){
+			const rSpeed=this.r+this.f*1.4;
+
+			const canIncresseX=this.x-rSpeed>0&&this.x+rSpeed<w;
+			const canIncresseY=this.y-rSpeed>0&&this.y+rSpeed<h;
+			if(this.r<minRPlusFactor&&
+				 canIncresseX&&
+				 canIncresseY)this.r+=this.f;
+		}else{
+			this.r = Math.max(this.minR, this.r - this.f);
 		}
 
 		this.x+=this.dx;
@@ -117,20 +153,21 @@ class Circle{
 }
 
 
-function AnimateCircles(){
-	requestAnimationFrame(AnimateCircles);
-	ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+function animateCircles(){
+	if(!pause)requestAnimationFrame(animateCircles);
+	ctx.clearRect(0,0,w,h);
 	for(const i of circles){
-		i.move(ctx);
+		i.move(ctx,pointer);
 	}
 }
 
 
 
 const circles=[];
-const startRadius=15;
+const startRadius=18;
+const pointer={x:-1000,y:-1000};
 
+let w,h,
+	pause=false;
 
-
-resizeCanvas();
-AnimateCircles();
+handleResize();
